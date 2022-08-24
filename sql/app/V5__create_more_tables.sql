a4015ef17aadac94d1b692df5cfefc80cb8707f0
Abel <joseabel298@gmail.com>
2022-08-23 20:10:31 -0600

    Update

--- sql/app/V5__create_more_tables.sql
+++ sql/app/V5__create_more_tables.sql
@@ -3,7 +3,7 @@
 CREATE TABLE ${flyway:database}.cliente(
     id int auto_increment NOT NULL,
     name  varchar(50) NOT NULL,
-    lastname  varchar(50) NOT NULL,
+    lastname varchar(50) NOT NULL,
     tm_fecha_creacion_registro timestamp NULL,
     tm_creado_por varchar(50)  NULL,
     tm_fecha_modificacion_registro timestamp NULL,
@@ -27,15 +27,15 @@ CREATE TRIGGER ${flyway:database}.cliente_update_aud
 
 CREATE TABLE ${flyway:database}.producto(
     id int auto_increment NOT NULL,
-    id_cliente int
-    nombre_producto  varchar(50) NOT NULL,
-    descripcion  varchar(50) NOT NULL,
+    id_cliente int,
+    nombre_producto varchar(50) NOT NULL,
+    descripcion varchar(50) NOT NULL,
     tm_fecha_creacion_registro timestamp NULL,
     tm_creado_por varchar(50)  NULL,
     tm_fecha_modificacion_registro timestamp NULL,
     tm_modificado_por varchar(50)  NULL,
-    CONSTRAINT producto_pkey PRIMARY KEY (id)
-    CONSTRAINT fk_cliente FOREIGN KEY id_cliente REFERENCES cliente(id)
+    CONSTRAINT producto_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id)
 
 );
 CREATE TRIGGER ${flyway:database}.producto_insert_aud
