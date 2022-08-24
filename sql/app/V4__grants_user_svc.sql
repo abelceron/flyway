ce52fa3c933cd54a631983c8dc264d4caa351c99
Abel <joseabel298@gmail.com>
2022-08-23 19:47:18 -0600

    update

--- sql/app/V4__grants_user_svc.sql
+++
@@ -1,5 +0,0 @@
-GRANT usage ON ${flyway:database}.* TO 'app_fclogic'@'%';
-GRANT update ON ${flyway:database}.* TO 'app_fclogic'@'%';
-GRANT delete ON ${flyway:database}.* TO 'app_fclogic'@'%';
-GRANT insert ON ${flyway:database}.* TO 'app_fclogic'@'%';
-GRANT select ON ${flyway:database}.* TO 'app_fclogic'@'%';
